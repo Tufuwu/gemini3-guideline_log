4402a5019392514fde5fb4ca9d80109c3128f23d
wu <wu@.com>
2026-01-30 01:23:38 +0800

    Logs-jungtaekkim/bayeso-benchmarks

--- setup.py
+++ setup.py
@@ -1,55 +1,25 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
+path_requirements = 'requirements.txt'
+list_packages = ['bayeso_benchmarks']
 
-def read_file(filename):
-    path = os.path.join(here, filename)
-    return open(path, encoding="utf-8").read() if os.path.exists(path) else ""
-
-README = read_file("README.rst")
-NEWS = read_file("NEWS.txt")
-
-version = "0.5.0"
-
-install_requires = [
-    "urllib3>=1.7.1",
-    "dnspython>=1.13.0",
-]
-
-extras_require = {
-    "test": [
-        "pytest",
-        "pytest-mock",
-        "pyOpenSSL>=0.14",
-    ]
-}
+with open(path_requirements) as f:
+    required = f.read().splitlines()
 
 setup(
-    name="python-etcd",
-    version=version,
-    description="A python client for etcd",
-    long_description=README + "\n\n" + NEWS,
-    long_description_content_type="text/x-rst",
+    name='bayeso-benchmarks',
+    version='0.1.6',
+    author='Jungtaek Kim',
+    author_email='jtkim@postech.ac.kr',
+    url='https://github.com/jungtaekkim/bayeso-benchmarks',
+    license='MIT',
+    description='Benchmarks for Bayesian optimization',
+    packages=list_packages,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, != 3.3.*, !=3.4.*, !=3.5.*, <4',
+    install_requires=required,
     classifiers=[
-        "Topic :: System :: Distributed Computing",
-        "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Database :: Front-Ends",
-    ],
-    keywords="etcd raft distributed log api client",
-    author="Jose Plana",
-    author_email="jplana@gmail.com",
-    url="http://github.com/jplana/python-etcd",
-    license="MIT",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-
-    # Modern testing: no nose, no test_suite
-    extras_require=extras_require,
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ]
 )
